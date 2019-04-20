4168d0b07f26c139499e466befaf3451229f9b3e
Geert Wirken <geert@gwirken.nl>
2019-04-20 17:28:33 +0200

    Added some tests for departures

---
+++ models/departure_test.go
@@ -0,0 +1,70 @@
+package models
+
+import (
+	"testing"
+	"time"
+)
+
+// TestRealDepartureTime tests the real departure time based on the planned time plus delay
+func TestRealDepartureTime(t *testing.T) {
+	var departure Departure
+
+	departure.DepartureTime, _ = time.Parse(time.RFC3339, "2019-01-27T12:34:00+01:00")
+
+	expectedTime, _ := time.Parse(time.RFC3339, "2019-01-27T12:34:00+01:00")
+
+	if !departure.RealDepartureTime().Equal(expectedTime) {
+		t.Errorf("Real departure time %v does not match expected value %v", departure.RealDepartureTime(), expectedTime)
+	}
+
+	departure.Delay = 30 // 30 seconds
+	expectedTime, _ = time.Parse(time.RFC3339, "2019-01-27T12:34:30+01:00")
+
+	if !departure.RealDepartureTime().Equal(expectedTime) {
+		t.Errorf("Real departure time %v does not match expected value %v", departure.RealDepartureTime(), expectedTime)
+	}
+
+	departure.Delay = 3600 // 1 hour
+	expectedTime, _ = time.Parse(time.RFC3339, "2019-01-27T13:34:00+01:00")
+
+	if !departure.RealDepartureTime().Equal(expectedTime) {
+		t.Errorf("Real departure time %v does not match expected value %v", departure.RealDepartureTime(), expectedTime)
+	}
+
+	departure.Delay = -120 // Negative: -2 minutes
+	expectedTime, _ = time.Parse(time.RFC3339, "2019-01-27T12:32:00+01:00")
+
+	if !departure.RealDepartureTime().Equal(expectedTime) {
+		t.Errorf("Real departure time %v does not match expected value %v", departure.RealDepartureTime(), expectedTime)
+	}
+}
+
+func TestPlatformChanged(t *testing.T) {
+	tables := []struct {
+		planned string
+		actual  string
+		changed bool
+	}{
+		{"", "", false},
+		{"4", "4", false},
+		{"4", "5", true},
+		{"", "4", true},
+		{"4", "", true},
+	}
+
+	for _, table := range tables {
+		var departure Departure
+		departure.PlatformPlanned = table.planned
+		departure.PlatformActual = table.actual
+
+		if table.changed {
+			if !departure.PlatformChanged() {
+				t.Errorf("Planned platform %s is different from actual platform %s, but not changed", departure.PlatformPlanned, departure.PlatformActual)
+			}
+		} else {
+			if departure.PlatformChanged() {
+				t.Errorf("Planned platform %s is equal to actual platform %s, but is marked as changed", departure.PlatformPlanned, departure.PlatformActual)
+			}
+		}
+	}
+}
