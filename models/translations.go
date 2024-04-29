45211327773de33724e7b45c43d40b3e3ed4eb4e
Geert Wirken <geert@gwirken.nl>
2024-04-29 22:18:30 +0200

    Add translation

--- models/translations.go
+++ models/translations.go
@@ -129,6 +129,7 @@ var causeTranslations = map[string]string{
 	"door een systeemstoring":                                            "due to a system failure",
 	"door noodzakelijke aanpassing in de dienstregeling":                 "due to a necessary timetable change",
 	"om veiligheidsredenen":                                              "for safety reasons",
+	"door een dienstregelingswijziging":                                  "due to a timetable change",
 }
 
 // Translate returns the appropriate translation based on language
